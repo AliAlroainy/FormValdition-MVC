6850e36461afc28aca967f409f222d763644596d
Ali <aliabduh.2020@gmail.com>
2022-03-24 15:03:18 +0300

    form valdtion Ali

---
+++ app/views/user_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .username{
+            color:red;
+            font-size: 2rem;
+        }
+    </style>
+
+</head>
+<body>
+   <div class="username"><?php echo $data ?></div> 
+</body>
+</html>
